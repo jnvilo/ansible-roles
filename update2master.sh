b70df0254e6c2af0420fa56fb8328a4db10ef17a
Jason Viloria <jason.viloria@optiscangroup.com>
2020-08-27 23:11:57 +0300

    updated submodules to master - 2020082020-2311

---
+++ update2master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+modules=$(ls | grep jnvilo)
+
+for module in $modules
+do
+	cd $module
+	git checkout master
+	cd ..
+done
+
+timestamp=`date +%Y%m%Y-%H%M`
+
+git commit -a -m "updated submodules to master - $timestamp"
